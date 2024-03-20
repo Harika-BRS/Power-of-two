143d6dffa4119b2d67aa911b88d0a4a15acc7005
Harika-BRS <153584946+Harika-BRS@users.noreply.github.com>
2024-03-20 12:27:48 +0530

    files uploaded

--- assets/app.js
+++ assets/app.js
@@ -10,6 +10,12 @@ document.getElementById("submit").addEventListener("click", function() {
     }
 });
 
+document.getElementById("intervalsInput").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        document.getElementById("submit").click();
+    }
+});
+
 /**
  * @param {number} n
  * @return {boolean}
@@ -18,9 +24,8 @@ var isPowerOfTwo = function(n) {
     return (n > 0 && (n & (n - 1)) === 0);
 };
 
-
 // Add this script to clear the input field on page load
 document.addEventListener('DOMContentLoaded', function() {
     var intervalsInput = document.getElementById('intervalsInput');
     intervalsInput.value = ''; // Set the input value to an empty string
-  });
+});
